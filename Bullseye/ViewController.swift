aaff695451bc1082d072170c089103fa8935e0cf
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-12 16:10:47 +1100

    Add result label component

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -13,12 +13,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var numLabel: UILabel!
     @IBOutlet weak var numSlider: UISlider!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     
     var counter = 0
+    var randomNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        randomNumber = Int(arc4random_uniform(101))
+        numLabel.text = "Move this slider to \(randomNumber)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,14 +31,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-    @IBAction func increase(_ sender: Any) {
-        counter += 1
-        numLabel.text = "\(counter)"
+    @IBAction func checkValue(_ sender: Any) {
+        if randomNumber == Int(numSlider.value) {
+            resultLabel.text = "You were on point! Bullseye!"
+        } else {
+            resultLabel.text = "Whoops! You missed! Try again later!"
+        }
     }
     
     @IBAction func sliderValueChanged(_ sender: Any) {
-        numLabel.text = "\(round(numSlider.value))"
+//        numLabel.text = "\(round(numSlider.value))"
     }
 }
 
